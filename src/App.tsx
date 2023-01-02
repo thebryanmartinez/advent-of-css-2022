47440169b069e694aa0d1bd90d732fa65a11b47e
Bryan Martinez <bryanmartz972@gmail.com>
2023-01-02 15:44:43 -0600

    ➕ Install react-router-dom and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+const Home = () => <h1>Home</h1>
+
 const App = () => {
-  return (
-    <>
-      <div className=''>Hello</div>
-    </>
-  )
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
+	)
 }
 
 export default App
